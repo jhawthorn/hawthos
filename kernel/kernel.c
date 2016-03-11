1e3bee2be18203cde5c22037b4d3d8ab69692cdf
John Hawthorn <john.hawthorn@gmail.com>
2016-03-10 22:52:38 -0800

    Print stuff to the screen

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,2 +1,26 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+volatile uint16_t *terminal_buffer = (uint16_t *)0xB8000;
+size_t terminal_pos = 0;
+
+void clear() {
+	for (size_t y = 0; y < VGA_HEIGHT; y++)
+		for (size_t x = 0; x < VGA_WIDTH; x++)
+			terminal_buffer[y * VGA_WIDTH + x] = 0;
+	terminal_pos = 0;
+}
+
+void puts(char *s) {
+	while(*s && terminal_pos < VGA_WIDTH * VGA_HEIGHT) {
+		terminal_buffer[terminal_pos++] = 0x0f00 | *s++;
+	}
+}
+
 void kernel_main() {
+	clear();
+	puts("Loaded HawthOS kernel");
 }
