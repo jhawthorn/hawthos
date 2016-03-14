1a1e8d0ecd3db4d339d1729fd75f075d7353c350
John Hawthorn <john.hawthorn@gmail.com>
2016-03-14 00:53:24 -0700

    Fix format of TSS

--- kernel/tss.c
+++ kernel/tss.c
@@ -6,18 +6,18 @@ typedef struct __attribute__((__packed__)) {
 
 	uint32_t esp0;
 
-	uint16_t _reserved1;
 	uint16_t ss0;
+	uint16_t _reserved1;
 
 	uint32_t esp1;
 
-	uint16_t _reserved2;
 	uint16_t ss1;
+	uint16_t _reserved2;
 
 	uint32_t esp2;
 
-	uint16_t _reserved3;
 	uint16_t ss2;
+	uint16_t _reserved3;
 
 	uint32_t cr3;
 	uint32_t eip;
@@ -31,26 +31,26 @@ typedef struct __attribute__((__packed__)) {
 	uint32_t esi;
 	uint32_t edi;
 
-	uint16_t _reserved4;
 	uint16_t es;
+	uint16_t _reserved4;
 
-	uint16_t _reserved5;
 	uint16_t cs;
+	uint16_t _reserved5;
 
-	uint16_t _reserved6;
 	uint16_t ss;
+	uint16_t _reserved6;
 
-	uint16_t _reserved7;
 	uint16_t ds;
+	uint16_t _reserved7;
 
-	uint16_t _reserved8;
 	uint16_t fs;
+	uint16_t _reserved8;
 
-	uint16_t _reserved9;
 	uint16_t gs;
+	uint16_t _reserved9;
 
-	uint16_t _reserved10;
 	uint16_t ldtr;
+	uint16_t _reserved10;
 
 	uint16_t iopb_offset;
 	uint16_t _reserved11;
