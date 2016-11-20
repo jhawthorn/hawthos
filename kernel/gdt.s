57869736fc50e782f498c0db6e1a042dc0f76dd4
John Hawthorn <john.hawthorn@gmail.com>
2016-11-20 04:02:15 -0800

    Add comments to gdt.s

--- kernel/gdt.s
+++ kernel/gdt.s
@@ -1,7 +1,18 @@
 .section .data
 
+# Segment Format:
+
+# 31           24                                 7             0
+#+---------------+-------+-------+---------------+---------------+
+#| Base (24-31)  | Flags |Limit' | Type          | Base (16-23)  |
+#+---------------+-------+-------+---------------+---------------+
+#+-------------------------------+-------------------------------+
+#| Base (0-15)                   | Limit (0-15)                  |
+#+-------------------------------+-------------------------------+
+#
+
 gdt:
-# 0x00 Reserved
+# 0x00 Reserved "null" descriptor
 .long 0x00000000
 .long 0x00000000
 # 0x08 Code - ring 0
@@ -16,7 +27,7 @@ gdt:
 # 0x20 Data - ring 3
 .long 0x0000FFFF
 .long 0x00CFF200
-# 0x28 TSS
+# 0x28 TSS (with address unset)
 gdt_tss:
 .long 0x00000068
 .long 0x00008900
@@ -31,6 +42,8 @@ gdt_descriptor:
 .global load_gdt
 load_gdt:
 # We need to add the TSS's address to the tss segment
+# The TSS segment needs to hold the address, but the way it is specified is too
+# awkward to do at link time.
 	mov $tss, %ecx
 	shl $16, %ecx
 	or %ecx, gdt_tss
@@ -45,8 +58,14 @@ load_gdt:
 
 # Load the GDT
 	lgdt gdt_descriptor
+# The GDT is now loaded, but for it to take effect, we need to reload the
+# segment registers.
+
+# ljmp into our new ring 0 code segment
 	ljmp $0x08, $reload_cs
 reload_cs:
+
+# Set all data selectors to the new ring 0 data segment
 	mov $0x10, %ax
 	mov %ax, %ds
 	mov %ax, %es
@@ -54,7 +73,7 @@ reload_cs:
 	mov %ax, %gs
 	mov %ax, %ss
 
-# Load TSS
+# Load the TSS via its descriptor in the gdt
 	mov $0x2b, %ax
 	ltr %ax
 
