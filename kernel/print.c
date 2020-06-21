ec91637ebefc22ccf3bad89099f6c2ca3d8d5f38
John Hawthorn <john@hawthorn.email>
2020-06-21 00:50:11 -0700

    Implement VGA cursor and scrolling

--- kernel/print.c
+++ kernel/print.c
@@ -2,9 +2,17 @@
 #include <stdint.h>
 
 #include "print.h"
+#include "io.h"
 
-volatile uint16_t *terminal_buffer = (uint16_t *)0xB8000;
-size_t terminal_pos = 0;
+static volatile uint16_t *terminal_buffer = (uint16_t *)0xB8000;
+static size_t terminal_pos = 0;
+
+static void update_cursor() {
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (uint8_t) (terminal_pos & 0xFF));
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (uint8_t) ((terminal_pos >> 8) & 0xFF));
+}
 
 void clear() {
 	for (size_t y = 0; y < VGA_HEIGHT; y++)
@@ -13,20 +21,35 @@ void clear() {
 	terminal_pos = 0;
 }
 
-void putchar(char c) {
-	if(terminal_pos < VGA_WIDTH * VGA_HEIGHT) {
-		if(c == '\n') {
-			terminal_pos += VGA_WIDTH - (terminal_pos % VGA_WIDTH);
-		} else {
-			terminal_buffer[terminal_pos++] = 0x0f00 | c;
+static void putchar_s(char c) {
+	if(c == '\n') {
+		do {
+			putchar(' ');
+		} while(terminal_pos % VGA_WIDTH);
+	} else {
+		terminal_buffer[terminal_pos++] = 0x0f00 | c;
+		if (terminal_pos == VGA_WIDTH * VGA_HEIGHT) {
+			for (int i = 0; i < VGA_WIDTH * (VGA_HEIGHT - 1); i++) {
+				terminal_buffer[i] = terminal_buffer[i + VGA_WIDTH];
+			}
+			for (int i = VGA_WIDTH * (VGA_HEIGHT - 1); i < VGA_WIDTH * VGA_HEIGHT; i++) {
+				terminal_buffer[i] = 0;
+			}
+			terminal_pos = VGA_WIDTH * (VGA_HEIGHT - 1);
 		}
 	}
 }
 
+void putchar(char c) {
+	putchar_s(c);
+	update_cursor();
+}
+
 void print(char *s) {
 	while(*s) {
-		putchar(*s++);
+		putchar_s(*s++);
 	}
+	update_cursor();
 }
 
 void printnum(uint32_t num, uint32_t radix) {
@@ -35,8 +58,9 @@ void printnum(uint32_t num, uint32_t radix) {
 	}
 	num %= radix;
 	if (num >= 10) {
-		putchar('A' + (num - 10));
+		putchar_s('A' + (num - 10));
 	} else {
-		putchar('0' + num);
+		putchar_s('0' + num);
 	}
+	update_cursor();
 }
