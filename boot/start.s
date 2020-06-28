6d0e1d05593f3efe4fd46ad31d5ad9a29b3cbf96
John Hawthorn <john@hawthorn.email>
2020-06-27 21:40:39 -0700

    Add C code to boot

--- boot/start.s
+++ boot/start.s
@@ -1,23 +1,8 @@
 .global _start
 _start:
 	movl $stack_top, %esp
-	push %eax
 
-	mov $0x1000, %eax
-	mov $'h', %ebx
-	int $0x80
-	mov $0x1000, %eax
-	mov $'e', %ebx
-	int $0x80
-	mov $0x1000, %eax
-	mov $'l', %ebx
-	int $0x80
-	mov $0x1000, %eax
-	mov $'l', %ebx
-	int $0x80
-	mov $0x1000, %eax
-	mov $'o', %ebx
-	int $0x80
+	call main
 loop:
 	jmp loop
 
