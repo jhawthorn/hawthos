c1abe52933ac826fc59cecd780e8771eb8c851c4
John Hawthorn <john@hawthorn.email>
2020-06-27 20:56:18 -0700

    Setup a stack for boot module

--- boot/start.s
+++ boot/start.s
@@ -1,5 +1,8 @@
 .global _start
 _start:
+	movl $stack_top, %esp
+	push %eax
+
 	mov $0x1001, %eax
 	mov $0x1002, %ebx
 	mov $0x1003, %ecx
@@ -7,3 +10,9 @@ _start:
 	int $0x80
 loop:
 	jmp loop
+
+.section .data
+.align 32
+stack_bottom:
+.skip 0x4000 # 16 KiB
+stack_top:
