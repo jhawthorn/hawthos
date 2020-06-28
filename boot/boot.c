a2758d75d0546378badb5e528680a489c94e2c9e
John Hawthorn <john@hawthorn.email>
2020-06-27 22:08:35 -0700

    Fix indentation

--- boot/boot.c
+++ boot/boot.c
@@ -2,7 +2,7 @@
 
 uint32_t syscall(uint32_t number, uint32_t arg1, uint32_t arg2, uint32_t arg3) {
 	uint32_t ret;
-    asm volatile ( "int $0x80" : "=a"(ret) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3) );
+	asm volatile ( "int $0x80" : "=a"(ret) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3) );
 	return ret;
 }
 
