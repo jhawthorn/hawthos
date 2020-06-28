372fe50d8f08bf2a223b3ff7e0dffb3d3ab35135
John Hawthorn <john@hawthorn.email>
2020-06-28 15:13:58 -0700

    Add returns to user syscalls

--- boot/boot.c
+++ boot/boot.c
@@ -1,13 +1,17 @@
 #include <stdint.h>
+#include <stddef.h>
 
-uint32_t syscall(uint32_t number, uint32_t arg1, uint32_t arg2, uint32_t arg3) {
+uint32_t syscall(uint32_t number, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t *out) {
+	uint32_t status;
 	uint32_t ret;
-	asm volatile ( "int $0x80" : "=a"(ret) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3) );
-	return ret;
+	asm volatile ( "int $0x80" : "=a"(status), "=b"(ret) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3) );
+	if (out)
+		*out = ret;
+	return status;
 }
 
 void putchar(char c) {
-	syscall(0x1000, c, 0, 0);
+	syscall(0x1000, c, 0, 0, NULL);
 }
 
 void puts(char *s) {
