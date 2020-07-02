3ba702275ae567345ef720edb786708755c9112b
John Hawthorn <john@hawthorn.email>
2020-07-02 12:29:57 -0700

    Handle not page aligned free sections

--- kernel/page_allocator.c
+++ kernel/page_allocator.c
@@ -24,26 +24,32 @@ void init_page_allocator(multiboot_info_t *multiboot_info) {
 	multiboot_memory_map_t *mmap = (multiboot_memory_map_t *) multiboot_info->mmap_addr;
 	multiboot_memory_map_t *mmap_end = (multiboot_memory_map_t *) (multiboot_info->mmap_addr + multiboot_info->mmap_length);
 
-	while (mmap < mmap_end && current_segment != 0) {
+	for (; mmap < mmap_end && current_segment != 0; mmap = (multiboot_memory_map_t *)((uintptr_t)mmap + mmap->size + 4)) {
 		if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE) {
 			uint32_t addr = mmap->addr;
 			uint32_t len = mmap->len;
 
-			if (addr % PAGE_SIZE || len % PAGE_SIZE) {
-				print("WARN: non-page-aligned free segment: ");
-				printnum(addr, 16);
-				print(" - ");
-				printnum(addr+len, 16);
-				print("\n");
-			} else {
-				struct memory_segment *seg = &free_segments[--current_segment];
-				seg->start = addr;
-				seg->length = len;
-				seg->current = addr + len;
+			if (addr % PAGE_SIZE) {
+				uint32_t diff = PAGE_SIZE - (addr % PAGE_SIZE);
+				if (len <= diff)
+					continue;
+				addr += diff;
+				len  -= diff;
 			}
-		}
 
-		mmap = (multiboot_memory_map_t *)((uintptr_t)mmap + mmap->size + 4);
+			if (len % PAGE_SIZE) {
+				len -= len % PAGE_SIZE;
+			}
+
+			if (len == 0) {
+				continue;
+			}
+
+			struct memory_segment *seg = &free_segments[--current_segment];
+			seg->start = addr;
+			seg->length = len;
+			seg->current = addr + len;
+		}
 	}
 }
 
