3eea252b271eb4acdf52135ae8613ffa8db39f39
John Hawthorn <john@hawthorn.email>
2020-07-02 01:07:45 -0700

    Swap success and failure

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -1,7 +1,7 @@
 uint32_t handle_syscall(uint32_t number, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t *ret);
 
-#define SYSCALL_FAILURE 0
-#define SYSCALL_SUCCESS 1
+#define SYSCALL_SUCCESS 0
+#define SYSCALL_FAILURE 1
 
 #define SYSCALL_PRINT      0x1000
 #define SYSCALL_ALLOC_PAGE 0x1001
