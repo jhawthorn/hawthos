2bcdb217c29d6d01f8cc31fa1422394b4dfd5598
John Hawthorn <john@hawthorn.email>
2020-07-02 00:49:11 -0700

    Implement more of stdio

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -1,2 +1,19 @@
+#define STDIN_FILENO  0
+#define STDOUT_FILENO  1
+#define STDERR_FILENO  1
+
+#define EOF -1
+
+typedef struct {
+	int fd;
+} FILE;
+
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
+
 int putchar(int c);
+int fputc(int c, FILE *stream);
+int putc(int c, FILE *stream);
 int puts(const char *s);
+int fputs(const char *s, FILE *stream);
--- libc/stdio.c
+++ libc/stdio.c
@@ -4,14 +4,45 @@
 
 #include <hawthos.h>
 
+static FILE _stdin  = { .fd = STDIN_FILENO };
+static FILE _stdout = { .fd = STDOUT_FILENO };
+static FILE _stderr = { .fd = STDERR_FILENO };
+
+FILE *stdin = &_stdin;
+FILE *stdout = &_stdout;
+FILE *stderr = &_stderr;
+
 int putchar(int c) {
-	syscall(0x1000, c, 0, 0, NULL);
+	if (syscall(0x1000, c, 0, 0, NULL))
+		return EOF;
 	return 0;
 }
 
-int puts(const char *s) {
-	while (*s) {
-		putchar(*s++);
+int fputc(int c, FILE *stream) {
+	if (stream == stdin || stream == stdout) {
+		if (putchar(c))
+			return EOF;
+		return 0;
+	} else {
+		return EOF;
 	}
+}
+
+int putc(int c, FILE *stream) {
+	return fputc(c, stream);
+}
+
+int puts(const char *s) {
+	while (*s)
+		if (putchar(*s++))
+			return EOF;
 	return 0;
+}
+
+int fputs(const char *s, FILE *stream) {
+	if (stream == stdin || stream == stdout) {
+		return puts(s);
+	} else {
+		return EOF;
+	}
 }
