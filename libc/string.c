c328766d47d5ade6125a30bb02fb89f19d1574ba
John Hawthorn <john@hawthorn.email>
2020-07-02 21:15:59 -0700

    Add memcpy

--- libc/string.c
+++ libc/string.c
@@ -7,3 +7,13 @@ void *memset(void *s, int c, size_t n) {
 	}
 	return s;
 }
+
+void *memcpy(void *_dest, const void *_src, size_t n) {
+	char *dest = _dest;
+	const char *src = _src;
+
+	while (n--)
+		*dest++ = *src++;
+
+	return _dest;
+}
