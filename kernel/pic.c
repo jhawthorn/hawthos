6639e2b83209c19879d4a973193c8962b32ed06c
John Hawthorn <john@hawthorn.email>
2020-07-20 21:22:38 -0700

    Fix words

--- kernel/pic.c
+++ kernel/pic.c
@@ -2,8 +2,8 @@
 
 #include "io.h"
 
-#define PIC1		0x20		/* IO base address for master PIC */
-#define PIC2		0xA0		/* IO base address for slave PIC */
+#define PIC1		0x20		/* IO base address for first PIC */
+#define PIC2		0xA0		/* IO base address for second PIC */
 #define PIC1_COMMAND	PIC1
 #define PIC1_DATA	(PIC1+1)
 #define PIC2_COMMAND	PIC2
@@ -17,8 +17,8 @@
 
 #define ICW4_8086	0x01		/* 8086/88 (MCS-80/85) mode */
 #define ICW4_AUTO	0x02		/* Auto (normal) EOI */
-#define ICW4_BUF_SLAVE	0x08		/* Buffered mode/slave */
-#define ICW4_BUF_MASTER	0x0C		/* Buffered mode/master */
+#define ICW4_BUF_1	0x08		/* Buffered mode/first */
+#define ICW4_BUF_2	0x0C		/* Buffered mode/second */
 #define ICW4_SFNM	0x10		/* Special fully nested (not) */
 
 static void pic_remap(uint8_t offset1, uint8_t offset2) {
@@ -29,10 +29,10 @@ static void pic_remap(uint8_t offset1, uint8_t offset2) {
 
 	outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
 	outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
-	outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
-	outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
-	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
-	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
+	outb(PIC1_DATA, offset1);                 // ICW2: First PIC vector offset
+	outb(PIC2_DATA, offset2);                 // ICW2: Second PIC vector offset
+	outb(PIC1_DATA, 4);                       // ICW3: tell first PIC that there is a second PIC at IRQ2 (0000 0100)
+	outb(PIC2_DATA, 2);                       // ICW3: tell second PIC its cascade identity (0000 0010)
 	outb(PIC1_DATA, ICW4_8086);
 	outb(PIC2_DATA, ICW4_8086);
 
