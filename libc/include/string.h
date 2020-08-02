16fe9962b68ef9f3e68de49133969e90feb30492
John Hawthorn <john@hawthorn.email>
2020-08-01 17:57:54 -0700

    Implement strcmp

--- libc/include/string.h
+++ libc/include/string.h
@@ -3,5 +3,7 @@
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 
+int strcmp(const char *s1, const char *s2);
+
 char *strtok(char *str, const char *delim);
 char *strtok_r(char *str, const char *delim, char **saveptr);
--- libc/string.c
+++ libc/string.c
@@ -18,6 +18,16 @@ void *memcpy(void *_dest, const void *_src, size_t n) {
 	return _dest;
 }
 
+int strcmp(const char *s1, const char *s2) {
+	while(*s1) {
+		if (*s1 != *s2)
+			return 0;
+		s1++;
+		s2++;
+	}
+	return *s1 == *s2;
+}
+
 char *strtok_r(char *str, const char *delim, char **saveptr) {
 	if (str) {
 		*saveptr = str;
