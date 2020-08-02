7123cd420ea47308eec497ea82175f07869b49b4
John Hawthorn <john@hawthorn.email>
2020-08-01 17:22:05 -0700

    Add missing malloc declarations

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -3,6 +3,11 @@
 int brk(void *addr);
 void *sbrk(intptr_t increment);
 
+void *malloc(size_t size);
+void *calloc(size_t size, size_t nmemb);
+void free(void *ptr);
+void *realloc(void *ptr, size_t size);
+
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 void exit(int status);
